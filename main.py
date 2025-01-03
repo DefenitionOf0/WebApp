a8b5dbfa67ab0125806d237e2fa9e35b21594792
DefenitionOf0 <nonokiyata@vk.com>
2025-01-03 11:18:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -113,8 +113,7 @@ if uploaded_file:
             )
 
     # Навигация
-    prev_contour = st.button("⬅ Предыдущий контур")
-    next_contour = st.button("Следующий контур ➡")
+    prev_contour, next_contour = st.button("⬅ Предыдущий контур"), st.button("Следующий контур ➡")st.button("Следующий контур ➡")
 
     if prev_contour:
         st.session_state.current_contour = max(0, st.session_state.current_contour - 1)
