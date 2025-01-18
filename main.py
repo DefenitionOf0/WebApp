8dc6b956b55a712705d7fc234f2d26b8d27694f5
DefenitionOf0 <nonokiyata@vk.com>
2025-01-18 23:29:53 +0500

    2222222222

--- main.py
+++ main.py
@@ -107,10 +107,11 @@ if uploaded_file:
     st.session_state.filtered_image = processor.apply_filters(blur, contrast, median_filter)
     processor.filtered_image = st.session_state.filtered_image
 
-    # Пересчёт контуров
-    st.session_state.all_original_contours = processor.process_image(
-        scaling_factor, tolerance, binary_thresh, adaptive_thresh
-    )
+    # Если контуры ещё не рассчитаны
+    if st.session_state.all_original_contours is None:
+        st.session_state.all_original_contours = processor.process_image(
+            scaling_factor, tolerance, binary_thresh, adaptive_thresh
+        )
 
     # Применение фильтров площади и периметра
     st.session_state.filtered_contours = processor.filter_contours(
